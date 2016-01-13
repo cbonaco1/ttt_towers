67de2be9882462609c9bfe4b5d69cc92d73e556a
ChristianAndrew <ChristianAndrew@aa.io>
2016-01-13 18:02:58 -0500

    Create invisible disks, and start replacing invisible disk with real disk.

--- hanoi.js-gh-pages/skeleton/js/bundle.js
+++ hanoi.js-gh-pages/skeleton/js/bundle.js
@@ -67,22 +67,38 @@
 	};
 
 	View.prototype.setupTowers = function() {
-	  console.log("entered");
-	  var $tower1 = $("<ul></ul>").addClass("tower-1");
-	  var $tower2 = $("<ul></ul>").addClass("tower-2");
-	  var $tower3 = $("<ul></ul>").addClass("tower-3");
+	  var $tower1 = $("<ul></ul>").addClass("0");
+	  var $tower2 = $("<ul></ul>").addClass("1");
+	  var $tower3 = $("<ul></ul>").addClass("2");
+
 	  for (var i=1; i<=3; i++) {
 	    var $disc = $("<li></li>");
 	    $disc.addClass("disk-" + i);
 	    $tower1.append($disc);
+
+	    $tower2.append($("<li></li>").addClass("invisible"));
+	    $tower3.append($("<li></li>").addClass("invisible"));
+	    // $tower3.append("<li></li>");
 	  }
 	  this.el.append($tower1);
 	  this.el.append($tower2);
 	  this.el.append($tower3);
 	};
 
-	View.prototype.render = function() {
-
+	View.prototype.render = function($tower, topDisk) {
+	  //replace the first child that is not a disk
+	  var disk = topDisk;
+	  for (var i=2; i>=0; i--) {
+	    var child = $($tower.children()[i]);
+	    if (child.hasClass("invisible")) {
+	      child.removeClass("invisible");
+	      var topdiskclass = topDisk.attr("class");
+	    }
+	  }
+	  // for (var i=2; i>=0; i--) {
+	  //   if ()
+	  // }
+	  // $tower.prepend(topDisk);
 	};
 
 	View.prototype.clickTower = function () {
@@ -91,12 +107,19 @@
 	    //second click
 	    if (this.fromTower) {
 	      var topDisk = this.fromTower.children()[0];
-	      debugger
 
+	      //validate move
+	      var startTowerIdx = parseInt(this.fromTower.attr('class'));
+	      var endTowerIdx = parseInt($tower.attr('class'));
+
+	      if (this.game.move(startTowerIdx, endTowerIdx)) {
+	        this.render($tower, topDisk);
+	      } else {
+	        alert("Invalid move!");
+	      }
 
 	      this.fromTower.removeClass("selected");
 	      this.fromTower = null;
-
 	    }
 	    //first click
 	    else {
